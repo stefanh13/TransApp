cada6eee875dbfc9773f343aa0ed0745c8982e16
stefanh13 <stefanh13@ru.is>
2014-05-05 12:13:14 +0000

    Translation klasi

---
+++ TransApp/Models/Translation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TransApp.Models
+{
+    public class Translation
+    {
+        public int tID { get; set; }
+        public int vID { get; set; }
+        public string translationDescription { get; set; }
+        public string translationText { get; set; }
+        public string translationLanguage { get; set; }
+        public DateTime translationTime { get; set; }
+    }
+}
