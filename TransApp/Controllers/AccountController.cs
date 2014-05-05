af1ca3caea61b2c337c516f6f2b071af6b295b36
Krillinn <krillinn@gmail.com>
2014-05-05 13:15:01 -0700

    Lots of stuff sorry guys

--- TransApp/Controllers/AccountController.cs
+++ TransApp/Controllers/AccountController.cs
@@ -78,7 +78,8 @@ namespace TransApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.UserName };
+                var user = new ApplicationUser() { UserName = model.UserName, 
+                email = model.email};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- TransApp/Models/AccountViewModels.cs
+++ TransApp/Models/AccountViewModels.cs
@@ -59,5 +59,9 @@ namespace TransApp.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string email { get; set; }
     }
 }
