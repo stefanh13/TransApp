1b1c978ec4436fd308785888c8a8767750fcbf76
stefanh13 <stefanh13@ru.is>
2014-05-05 13:48:51 +0000

    UserRequestController

---
+++ TransApp/Controllers/UserRequestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TransApp.Controllers
+{
+    public class UserRequestController : Controller
+    {
+        //
+        // GET: /UserRequest/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
