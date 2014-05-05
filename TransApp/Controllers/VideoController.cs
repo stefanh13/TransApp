8069de5e4524b41191013fe418fd659e43ebb08a
stefanh13 <stefanh13@ru.is>
2014-05-05 16:12:03 +0000

    Byrja a testi

--- TransApp/Controllers/VideoController.cs
+++ TransApp/Controllers/VideoController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TransApp.Repositories;
 
 namespace TransApp.Controllers
 {
     public class VideoController : Controller
     {
+
+        private readonly IVideoRepository repo;
+
+        public VideoController(IVideoRepository rep)
+        {
+            repo = rep;
+        }
+        
+        
         //
         // GET: /Video/
         public ActionResult FrontPage()
