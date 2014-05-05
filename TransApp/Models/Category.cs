13806ac03ea9cc62f562cf72534d717af6eca0f7
stefanh13 <stefanh13@ru.is>
2014-05-05 12:15:52 +0000

    Category klasi

---
+++ TransApp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TransApp.Models
+{
+    public class Category
+    {
+        public int catID { get; set; }
+        public string categoryType { get; set; }
+
+    }
+}
