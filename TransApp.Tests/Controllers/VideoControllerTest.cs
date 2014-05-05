83b46c33e709c54b51159ecf1d97250354e7c107
stefanh13 <stefanh13@ru.is>
2014-05-05 14:56:01 +0000

    Bua til VideoController test

---
+++ TransApp.Tests/Controllers/VideoControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TransApp.Tests.Controllers
+{
+    [TestClass]
+    public class VideoControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        
+        }
+    }
+}
