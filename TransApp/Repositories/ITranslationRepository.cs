15b05ae8149bb7491f42133e0812dc467887736f
stefanh13 <stefanh13@ru.is>
2014-05-05 14:46:15 +0000

    Bua til TranslationRepository interface

---
+++ TransApp/Repositories/ITranslationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransApp.Models;
+
+namespace TransApp.Repositories
+{
+    public interface ITranslationRepository
+    {
+        IQueryable<Translation> GetTranslations();
+    }
+}
