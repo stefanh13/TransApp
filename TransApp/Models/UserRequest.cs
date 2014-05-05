5c7511612954b84a19f6b5ba900b0b04d967db6c
stefanh13 <stefanh13@ru.is>
2014-05-05 12:07:34 +0000

    UserRequest klasi

---
+++ TransApp/Models/UserRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TransApp.Models
+{
+    public class UserRequest
+    {
+        public int urID { get; set; }
+        public int uID { get; set; }
+        public string requestName { get; set; }
+        public string requestText { get; set; }
+        public string requestLanguage { get; set; }
+        public int likes { get; set; }
+        public DateTime requestTime { get; set; }
+    }
+}
