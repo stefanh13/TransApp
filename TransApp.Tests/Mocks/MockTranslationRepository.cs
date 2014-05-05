c44ea6884d720daeceb058fc191c3909577994a6
stefanh13 <stefanh13@ru.is>
2014-05-05 14:59:53 +0000

    Bua til Mock fyrir TranslationRepository

---
+++ TransApp.Tests/Mocks/MockTranslationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransApp.Repositories;
+
+namespace TransApp.Tests.Mocks
+{
+    class MockTranslationRepository : ITranslationRepository
+    {
+
+        public IQueryable<Models.Translation> GetTranslations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
