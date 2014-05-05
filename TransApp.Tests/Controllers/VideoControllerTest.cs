e8b6f3bbfb5f4a330dcc917c9a30b64a575e9120
stefanh13 <stefanh13@ru.is>
2014-05-05 16:12:41 +0000

    byrja a testi

--- TransApp.Tests/Controllers/VideoControllerTest.cs
+++ TransApp.Tests/Controllers/VideoControllerTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransApp.Models;
+using System.Collections.Generic;
+using TransApp.Controllers;
 
 namespace TransApp.Tests.Controllers
 {
@@ -7,8 +10,22 @@ namespace TransApp.Tests.Controllers
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddingNewVideoInEmptyTable()
         {
+            // Arrange:
+            List<Video> video = new List<Video>();
+            video.Add(new Video
+            {
+                vID = 1,
+                catID = 1,
+                videoName = "Kalli"
+            });
+
+            var mockRepo = new Mocks.MockVideoRepository(video);
+            var controller = new VideoController(mockRepo);
+            // Act:
+
+            // Assert:
         }
     }
 }
