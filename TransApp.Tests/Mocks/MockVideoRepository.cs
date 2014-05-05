e112818edad8fa991cea57d0455f9688c254802b
stefanh13 <stefanh13@ru.is>
2014-05-05 15:55:54 +0000

    Mocks taka 2

---
+++ TransApp.Tests/Mocks/MockVideoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransApp.Models;
+using TransApp.Repositories;
+
+namespace TransApp.Tests.Mocks
+{
+    class MockVideoRepository : IVideoRepository
+    {
+
+        private readonly List<Video> videos;
+        
+        
+        public MockVideoRepository(List<Video> vids)
+        {
+            videos = vids;
+        }
+        
+        public IQueryable<Models.Video> GetVideos()
+        {
+            return videos.AsQueryable();
+        }
+    }
+}
