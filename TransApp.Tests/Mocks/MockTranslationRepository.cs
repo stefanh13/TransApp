ede90d521a91d9bb95fa3f67843559f6a9fee25e
stefanh13 <stefanh13@ru.is>
2014-05-05 19:51:05 +0000

    Bua til mock fyrir translation repository

---
+++ TransApp.Tests/Mocks/MockTranslationRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransApp.Models;
+using TransApp.Repositories;
+
+namespace TransApp.Tests.Mocks
+{
+    class MockTranslationRepository : ITranslationRepository
+    {
+
+        private readonly List<Translation> translations;
+        
+        public MockTranslationRepository(List<Translation> transl)
+        {
+            translations = transl;
+        }
+
+        public IQueryable<Models.Translation> GetTranslations()
+        {
+            return translations.AsQueryable();
+        }
+    }
+}
