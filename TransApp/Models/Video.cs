239088b14a626c3afac201834260e016feed3554
stefanh13 <stefanh13@ru.is>
2014-05-05 12:14:50 +0000

    Video klasi

---
+++ TransApp/Models/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TransApp.Models
+{
+    public class Video
+    {
+        public int vID { get; set; }
+        public int catID { get; set; }
+        public string videoName { get; set; }
+
+    }
+}
