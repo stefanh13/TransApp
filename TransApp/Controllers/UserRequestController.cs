3875a0389bc08c059ecbe7e0d7be972fdf498e1d
haftor2410 <haftor.g@gmail.com>
2014-05-06 14:31:28 +0000

    entity framework installed

--- TransApp/Controllers/UserRequestController.cs
+++ TransApp/Controllers/UserRequestController.cs
@@ -8,7 +8,15 @@ namespace TransApp.Controllers
 {
     public class UserRequestController : Controller
     {
-        //
+        //TODO:
+        // 1 - Install package Entity framework
+        // 2 - Model class
+        // 3 - Context class
+        // 4 - Initializer class
+        // 5 - Reference initalizer to web config
+        // 6 - Add connection to web config
+        // 7 - Create scaffolding from model and data context
+
         // GET: /UserRequest/
         public ActionResult Index()
         {
