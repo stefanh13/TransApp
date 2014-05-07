f57230c864d34a918119d57eecba75fa06c597ac
stefanh13 <stefanh13@ru.is>
2014-05-07 20:23:05 +0000

    TranslationInitializer

---
+++ TransApp/DAL/TranslationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using TransApp.Models;
+
+namespace TransApp.DAL
+{
+    public class TranslationContext : DbContext
+    {
+         public TranslationContext() : base("TranslationContext")
+        {
+
+        }
+
+        public DbSet<Translation> translations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
