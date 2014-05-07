675e1a556dcfaa25deac7107be40372719f8868f
stefanh13 <stefanh13@ru.is>
2014-05-07 20:31:40 +0000

    TranslationInitializer

--- TransApp/DAL/TranslationInitializer.cs
+++ TransApp/DAL/TranslationInitializer.cs
@@ -2,10 +2,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TransApp.Models;
 
 namespace TransApp.DAL
 {
-    public class TranslationInitializer
+    public class TranslationInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<TranslationContext>
     {
+        protected override void Seed(TranslationContext context)
+        {
+            var translations = new List<Translation>
+            {
+                new Translation
+                {
+                    vID = 1,
+                    translationDescription = "Lorem ipsum",
+                    translationText = "Lorem ipsum",
+                    translationLanguage = "Enska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+                new Translation
+                {
+                    vID = 1,
+                    translationDescription = "Lorem ipsum Loreum",
+                    translationText = "Lorem ipsum ipsum",
+                    translationLanguage = "Franska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+                 new Translation
+                {
+                    vID = 1,
+                    translationDescription = "Lorem ipsum",
+                    translationText = "Lorem ipsum",
+                    translationLanguage = "Íslenska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+                 new Translation
+                {
+                    vID = 2,
+                    translationDescription = "Lorem ipsum",
+                    translationText = "Lorem ipsum",
+                    translationLanguage = "Íslenska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+                 new Translation
+                {
+                    vID = 3,
+                    translationDescription = "Lorem ipsum",
+                    translationText = "Lorem ipsum",
+                    translationLanguage = "Enska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+                 new Translation
+                {
+                    vID = 4,
+                    translationDescription = "Lorem ipsum",
+                    translationText = "Lorem ipsum",
+                    translationLanguage = "Íslenska",
+                    translationTime = DateTime.Parse("2013-03-03")
+
+                },
+            };
+
+            translations.ForEach(t => context.translations.Add(t));
+            context.SaveChanges();
+        }
     }
 }
