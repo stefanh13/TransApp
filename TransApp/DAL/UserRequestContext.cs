d14aeec62a0e64f1b666f2c650cc9ee6089127b8
haftor2410 <haftor.g@gmail.com>
2014-05-07 11:54:13 +0000

    UserReqContext class writen

--- TransApp/DAL/UserRequestContext.cs
+++ TransApp/DAL/UserRequestContext.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using TransApp.Models;
 
 namespace TransApp.DAL
 {
-    public class UserRequestContext
+    public class UserRequestContext : DbContext
     {
+        public UserRequestContext()
+            : base("UserRequestContext")
+        {
+
+        }
+        public DbSet<UserRequest> userRequest { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
