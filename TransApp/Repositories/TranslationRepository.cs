a3e806ab9bedb83ddd05d0d0b153694b65cf9bef
stefanh13 <stefanh13@ru.is>
2014-05-07 20:34:47 +0000

    TranslationRepository

---
+++ TransApp/Repositories/TranslationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TransApp.DAL;
+using TransApp.Models;
+
+namespace TransApp.Repositories
+{
+    public class TranslationRepository
+    {
+        TranslationContext translationDb = new TranslationContext();
+
+        public IEnumerable<Translation> GetAllTranslations()
+        {
+            return translationDb.translations;
+        }
+    }
+}
