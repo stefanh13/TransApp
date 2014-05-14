8051d0327037bf2e28e8aca5961f1b1bab53d80b
Krillinn <krillinn@gmail.com>
2014-05-14 08:48:04 -0700

    addedComment empty

--- TransApp/Scripts/Paging.js
+++ TransApp/Scripts/Paging.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+
     console.log("Java")
     $("#SearchID").keyup(function () {
         console.log("KEYUP")
@@ -15,6 +16,26 @@
             $("#SearchBtnID").attr('disabled', 'disabled');
         } else {
             $("#SearchBtnID").removeAttr('disabled');
+        }
+    });
+
+
+    console.log("Java2")
+    $("#CommentID").keyup(function () {
+        console.log("KEYUP2")
+        var empty = false;
+        $("#CommentID").each(function () {
+            console.log("in each function");
+            if ($(this).val().length == 0) {
+                console.log("is empty?");
+                empty = true;
+            }
+        });
+        console.log(empty + " empty?");
+        if (empty) {
+            $("CommhBtnID").attr('disabled', 'disabled');
+        } else {
+            $("#CommhBtnID").removeAttr('disabled');
         }
     });
 
