5170acdd0f3098bf9a679742594a5656c6c306c1
Krillinn <krillinn@gmail.com>
2014-05-14 11:24:30 -0700

    bla

--- TransApp/Scripts/Paging.js
+++ TransApp/Scripts/Paging.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     console.log("Java")
+
     $("#SearchID").keyup(function () {
         console.log("KEYUP")
         var empty = false;
@@ -31,28 +32,19 @@
         });
         console.log(empty + " empty?");
         if (empty) {
-            $("CommhBtnID").attr('disabled', 'disabled');
+            $("#CommhBtnID").attr('disabled', 'disabled');
         } else {
             $("#CommhBtnID").removeAttr('disabled');
         }
     });
 
-<<<<<<< HEAD
     $(function () {
         $('#back-btn').click(function () {
             window.history.back();
         });
     })
-=======
-    $("#fileInput").keyup(function () {
-        console.log("I");
-       // alert(this.files[0].size / 1024 / 1024);
-    });
 
     function checkFile() {
         console.log($("#fileInput"));
     };
-
-
->>>>>>> Like-sort
 });
