fc0a4f1757527ad53d3f5d96d5d07bd88635bd97
haftor2410 <haftor.g@gmail.com>
2014-05-19 15:22:57 +0000

    isl + sma fix

--- TransApp/Models/AccountViewModels.cs
+++ TransApp/Models/AccountViewModels.cs
@@ -17,14 +17,14 @@ namespace TransApp.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Lykilorðið verður að vera allavega {2} stafir.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Compare("NewPassword", ErrorMessage = "Nýja lykilorðið og staðfesta lykilorðið passa ekki.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -50,18 +50,18 @@ namespace TransApp.Models
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Lykilorðið verður að vera allavega {2} stafir..", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "Nýja lykilorðið og staðfesta lykilorðið passa ekki.")]
         public string ConfirmPassword { get; set; }
 
         [Required]
-        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "Invalid Email Address")]
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "Sláðu inn gilt tölvupóstfang")]
         [Display(Name = "Email Address")]
         public string email { get; set; }
     }
